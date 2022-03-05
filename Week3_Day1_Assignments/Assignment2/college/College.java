580e6643221063fe9bad5067e2b65a8702029956
raghuram4code <100080021+raghuram4code@users.noreply.github.com>
2022-03-06 04:24:41 +1100

    Add files via upload

---
+++ Week3_Day1_Assignments/Assignment2/college/College.java
@@ -0,0 +1,24 @@
+package org.college;
+
+public class College {
+	public String collgeName() {
+		String collegeName = "PEC";
+		return collegeName;
+
+	}
+	
+	protected String collegeCode() {
+		String collegeCode = "72345";
+		return collegeCode;
+
+	}
+	
+	protected char CollegeRank() {
+		// TODO Auto-generated method stub
+		char rank ='5';
+		return rank;
+	}
+	
+	
+
+}
---
+++ Week3_Day1_Assignments/Assignment2/department/Department.java
@@ -0,0 +1,13 @@
+package org.department;
+
+import org.college.College;
+
+public class Department extends College{
+
+	protected String depName() {
+		// TODO Auto-generated method stub
+		String dep ="ECE";
+		return dep;
+		
+	}
+}
